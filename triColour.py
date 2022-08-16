6cd0d15d2f57b47a5b9e5bfe795080e6f3765c20
dkswami <dkswami1820@gmail.com>
2022-08-16 11:55:33 +0530

    added color strips

--- triColour.py
+++ triColour.py
@@ -1,5 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as py
+from matplotlib import pyplot as py
 import matplotlib.patches as patch
 
 #Plotting the tri colours in national flag
+a = patch.Rectangle((0,1), width=12, height=2, facecolor='green', edgecolor='grey')
+b = patch.Rectangle((0,3), width=12, height=2, facecolor='white', edgecolor='grey')
+c = patch.Rectangle((0,5), width=12, height=2, facecolor='#FF6103', edgecolor='grey')
+
+m,n = py.subplots()
+n.add_patch(a)
+n.add_patch(b)
+n.add_patch(c)
+
+py.axis('equal')
+py.show()
