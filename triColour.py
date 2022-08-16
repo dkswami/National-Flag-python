8a097f5e12003c0376a80f90a7bb477100d6fa8d
dkswami <dkswami1820@gmail.com>
2022-08-16 12:24:13 +0530

    National flag completed with 24 spokes of ashokaChakra

--- triColour.py
+++ triColour.py
@@ -1,7 +1,8 @@
 from textwrap import fill
-import numpy as np
+from turtle import color
+import numpy as npy
 from matplotlib import pyplot as pplot
-import matplotlib.patches as patch
+from matplotlib import patches as patch
 
 #Plotting the tri colours in national flag
 greenstrip = patch.Rectangle((0,1), width=12, height=2, facecolor='green', edgecolor='grey')
@@ -18,6 +19,16 @@ radius=0.8
 pplot.plot(6,4, marker = 'o', markerfacecolor="#000089ff", markersize=9.5)
 ashokchakra = pplot.Circle((6,4), radius, color='#000089ff', fill=False, linewidth=7)
 n.add_artist(ashokchakra)
+
+#24 spokes inside AshokChakra
+for i in range(0,24):
+    a = 6 + radius/2 * npy.cos(npy.pi*i/12 + npy.pi/48)
+    b = 6 + radius/2 * npy.cos(npy.pi*i/12 - npy.pi/48)
+    c = 4 + radius/2 * npy.sin(npy.pi*i/12 + npy.pi/48)
+    d = 4 + radius/2 * npy.sin(npy.pi*i/12 - npy.pi/48)
+    e = 6 + radius * npy.cos(npy.pi*i/12)
+    f = 4 + radius * npy.sin(npy.pi*i/12)
+    n.add_patch(patch.Polygon([[6,4],[a,c],[e,f],[b,d]], fill=True, closed=True, color='#000089ff'))
 
 pplot.axis('equal')
 pplot.show()
